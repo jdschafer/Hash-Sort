9647922fdbb13532c373bf68a36ecdbb7a6c0d26
jdschafer <jdschafer@WC-002-08.ONU-AD.olivet.edu>
2015-04-30 16:48:44 -0500

    I can't even right now

---
+++ src/Runner.java
@@ -0,0 +1,27 @@
+/*@author       Justin Schafer
+ *@id           jdschafer
+ *@course       CSIS 252
+ *@assignment   Hash Sort Lab 6
+ *@related      
+ *@included     N/A
+ */
+
+import java.util.*;
+
+public class Runner {
+    
+    public UnsortedList unsorted;
+    public SortedList sorted;
+    public Hashtable hashtable;
+    
+    public Runner() {
+        unsorted = new UnsortedList();
+        sorted = new SortedList();
+        hashtable = new Hashtable<Integer, Object>();
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
